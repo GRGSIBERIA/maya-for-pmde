29ed90431261bff84a34b271996642be12ba8ed2
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-03-13 04:23:17 +0900

    implemented initialit

--- maya/importer/base.py
+++ maya/importer/base.py
@@ -1,4 +1,4 @@
-class ImporterBase
+class UnitBase
   def __init__(self):
     pass
 
--- maya/importer/vertex.py
+++ maya/importer/vertex.py
@@ -1,9 +1,22 @@
-class Vertex:
+class Vertex(UnitBase):
   def __init__(self, line):
     args = line.split(',')
-    index = int(args[1])
-    pos = self.parsef3(args, 2)
-    normal = self.parsef3(args, 5)
-    edge_pow = int(args[8])
-    uv = self.parsef2(args, 9)
+    self.index    = int(args[1])
+    self.pos      = UnitBase.parsef3(self, args, 2)
+    self.normal   = UnitBase.parsef3(self, args, 5)
+    self.edge_pow = int(args[8])
+    self.uv       = UnitBase.parsef2(self, args, 9)
+    self.add_uv   = [
+      UnitBase.parsef4(self, args, 11), UnitBase.parsef4(self, args, 15), 
+      UnitBase.parsef4(self, args, 19), UnitBase.parsef4(self, args, 23)
+    ]
+    self.weight_type  = int(args[27])
+    self.bone_names   = [args[28], args[30], args[32], args[34]]
+    self.bone_weights = [args[29], args[31], args[33], args[35]]
+    self.sdef_c = UnitBase.parsef3(self, args, 36)
+    self.sdef_r0 = UnitBase.parsef3(self, args, 39)
+    self.sdef_r1 = UnitBase.parsef3(self, args, 42)
+
+  def to_line(self):
+    args = []
     
